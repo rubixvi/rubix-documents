591e8fd6587b53f30d39d8f485d44f606da55b8f
Vincent Vu <vincent@rubixstudios.com.au>
2024-09-30 15:20:27 +1000

    Refactor theme toggle icons for better visibility

--- components/navigation/theme-toggle.tsx
+++ components/navigation/theme-toggle.tsx
@@ -22,8 +22,8 @@ export function ModeToggle() {
           "h-9 w-9"
       )}
     >
-      <RxSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <RxMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <RxSun className="w-[1.1rem] h-[1.1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <RxMoon className="absolute w-[1.1rem] h-[1.1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
