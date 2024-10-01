79e737aad7a93b1b796db2708e2d9065a163681c
Vincent Vu <vincent@rubixstudios.com.au>
2024-10-01 15:54:35 +1000

    Refactor component imports and file structure

--- components/navigation/anchor.tsx
+++ components/navigation/anchor.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { ComponentProps } from "react";
-import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 
