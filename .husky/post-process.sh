8928534d9cd7699c737f4f343aa08a1f454040b9
Vincent Vu <vincent@rubixstudios.com.au>
2024-10-01 21:16:11 +1000

    Refactor import paths, rename files to .mjs, and update TypeScript compilation in .husky scripts

--- .husky/post-process.sh
+++ .husky/post-process.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+echo "Starting TypeScript compilation..."
 npx tsc --project tsconfig.scripts.json
+echo "TypeScript compilation complete."
 
+echo "Renaming .js files to .mjs..."
 find dist/scripts -name "*.js" | while read file; do
   mv -f "$file" "${file%.js}.mjs"
   if [ $? -ne 0 ]; then
     echo "Error renaming $file"
     exit 1
   fi
+  echo "Renamed $file to ${file%.js}.mjs"
 done
 
 for file in dist/scripts/scripts/content.mjs dist/scripts/lib/pageroutes.mjs; do
