a2fe512b83067c67113032bf8d285dfd69470f41
Vincent Vu <vincent@rubixstudios.com.au>
2024-10-01 16:51:05 +1000

    Refactor post-process.sh to enable globstar and rename .js files to .mjs

--- .husky/post-process.sh
+++ .husky/post-process.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
+# Enable globstar for recursive glob patterns
+shopt -s globstar
+
+# Compile TypeScript project
 npx tsc --project tsconfig.scripts.json
 
+# Rename all .js files to .mjs
 for file in dist/scripts/**/*.js; do
-  mv "$file" "${file%.js}.mjs"
+  if [ -f "$file" ]; then
+    new_file="${file%.js}.mjs"
+    echo "Renaming $file to $new_file..."
+    mv "$file" "$new_file"
+    
+    if [ $? -ne 0 ]; then
+      echo "Error: Failed to rename $file"
+      exit 1
+    fi
+  else
+    echo "$file not found!"
+  fi
 done
 
 for file in dist/scripts/scripts/content.mjs dist/scripts/lib/pageroutes.mjs; do
