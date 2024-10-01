d8a0e027e0d8f67824b50d1085bf0ed89123b674
Vincent Vu <vincent@rubixstudios.com.au>
2024-10-01 21:24:45 +1000

    Refactor import paths, rename files to .mjs, and update import statements in content.mjs and pageroutes.mjs

---
+++ dist/scripts/lib/pageroutes.mjs
@@ -0,0 +1,23 @@
+import { Documents } from '../settings/documents';
+export const Routes = [
+    ...Documents,
+];
+function isRoute(node) {
+    return "title" in node && "href" in node;
+}
+function getAllLinks(node) {
+    const pages = [];
+    if (isRoute(node) && !node.noLink) {
+        pages.push({ title: node.title, href: node.href });
+    }
+    if (isRoute(node) && node.items) {
+        node.items.forEach((subNode) => {
+            if (isRoute(subNode)) {
+                const temp = { ...subNode, href: `${node.href}${subNode.href}` };
+                pages.push(...getAllLinks(temp));
+            }
+        });
+    }
+    return pages;
+}
+export const PageRoutes = Routes.map((it) => getAllLinks(it)).flat();
