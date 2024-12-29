8da13398200f83e151e90a01210756e469513f1c
Vincent Vu <vincent@rubixstudios.com.au>
2024-12-30 08:25:07 +1100

    Refactor pageroutes and documents: improve code formatting and structure for better readability

--- dist/scripts/lib/pageroutes.mjs
+++ dist/scripts/lib/pageroutes.mjs
@@ -1,22 +1,21 @@
-import { Documents } from "../settings/documents.mjs"
-
-export const Routes = [...Documents]
+import { Documents } from "../settings/documents.mjs";
+export const Routes = [...Documents];
 function isRoute(node) {
-  return "title" in node && "href" in node
+    return "title" in node && "href" in node;
 }
 function getAllLinks(node) {
-  const pages = []
-  if (isRoute(node) && !node.noLink) {
-    pages.push({ title: node.title, href: node.href })
-  }
-  if (isRoute(node) && node.items) {
-    node.items.forEach((subNode) => {
-      if (isRoute(subNode)) {
-        const temp = { ...subNode, href: `${node.href}${subNode.href}` }
-        pages.push(...getAllLinks(temp))
-      }
-    })
-  }
-  return pages
+    const pages = [];
+    if (isRoute(node) && !node.noLink) {
+        pages.push({ title: node.title, href: node.href });
+    }
+    if (isRoute(node) && node.items) {
+        node.items.forEach((subNode) => {
+            if (isRoute(subNode)) {
+                const temp = { ...subNode, href: `${node.href}${subNode.href}` };
+                pages.push(...getAllLinks(temp));
+            }
+        });
+    }
+    return pages;
 }
-export const PageRoutes = Routes.map((it) => getAllLinks(it)).flat()
+export const PageRoutes = Routes.map((it) => getAllLinks(it)).flat();
