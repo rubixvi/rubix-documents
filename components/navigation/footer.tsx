aec857e5b8c18bc70fc39139e9046278ffa99a49
Vincent Vu <172068404+rubixvi@users.noreply.github.com>
2025-10-02 19:24:16 +1000

    fix: correct footer padding for improved layout

--- components/navigation/footer.tsx
+++ components/navigation/footer.tsx
@@ -5,7 +5,7 @@ import { Settings } from "@/types/settings"
 
 export function Footer() {
   return (
-    <footer className="text-foreground lg:px- flex h-16 w-full flex-wrap items-center justify-center gap-4 border-t px-2 py-3 text-sm sm:justify-between sm:gap-0 sm:px-4 sm:py-0">
+    <footer className="text-foreground flex h-16 w-full flex-wrap items-center justify-center gap-4 border-t px-2 py-3 text-sm sm:justify-between sm:gap-0 sm:px-4 sm:py-0 lg:px-8">
       <p className="items-center">
         &copy; {new Date().getFullYear()}{" "}
         <Link
